0171e2eaf747dc89bd45c0ba3b289b1430d53a12
Aidan Elliot <aidanelliot@icloud.com>
2023-10-13 13:17:19 -0400

    Added dice svg's to main page to see how they look

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import logo from './logo.svg';
+import dice1 from './Assets/dice-six-faces-one.svg';
+import dice2 from './Assets/dice-six-faces-two.svg';
+import dice3 from './Assets/dice-six-faces-three.svg';
+import dice4 from './Assets/dice-six-faces-four.svg';
+import dice5 from './Assets/dice-six-faces-five.svg';
+import dice6 from './Assets/dice-six-faces-six.svg';
 import './App.css';
 import '@fontsource/inter';
 
@@ -7,7 +12,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+        <img src={dice1} className="dice1" alt="die1" />
+        <img src={dice2} className="dice2" alt="diee2" />
+        <img src={dice3} className="dice3" alt="die3" />
+        <img src={dice4} className="dice4" alt="die4" />
+        <img src={dice5} className="dice5" alt="die5" />
+        <img src={dice6} className="dice6" alt="die6" />
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
